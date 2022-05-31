ecf8374bc69f936f733569449a44ba3e325f1f68
sncap <sncap7@gmail.com>
2022-05-31 13:49:40 +0900

    Update push_img.sh

--- swa/push_img.sh
+++ swa/push_img.sh
@@ -3,18 +3,18 @@ echo "Start Docker images push"
 
 docker images | grep swa
 docker login
-docker tag swa/3rd_party_application/cancer:v1.1 sncap/3rd_party_app_cancer:v1.2
-docker push sncap/3rd_party_app_cancer:v1.2
-docker tag swa/data_source/cancer:v1.1 sncap/data_source_cancer:v1.2
-docker push sncap/data_source_cancer:v1.2
-docker tag swa/data_source/cifar10:v1.1 sncap/data_source_cifar10:v1.2
-docker push sncap/data_source_cifar10:v1.2
-docker tag swa/3rd_party_application/cifar10:v1.1 sncap/3rd_party_app_cifar10:v1.2
-docker push sncap/3rd_party_app_cifar10:v1.2
-docker tag swa/data_processing/cancer:v1.1 sncap/data_processing_cancer:v1.2
-docker push sncap/data_processing_cancer:v1.2
-docker tag swa/data_processing/cifar10:v1.1 sncap/data_processing_cifar10:v1.2
-docker push sncap/data_processing_cifar10:v1.2
+docker tag swa/3rd_party_application/cancer:v1.1 sncap/3rd_party_app_cancer:v1.3
+docker push sncap/3rd_party_app_cancer:v1.3
+docker tag swa/data_source/cancer:v1.1 sncap/data_source_cancer:v1.3
+docker push sncap/data_source_cancer:v1.3
+docker tag swa/data_source/cifar10:v1.1 sncap/data_source_cifar10:v1.3
+docker push sncap/data_source_cifar10:v1.3
+docker tag swa/3rd_party_application/cifar10:v1.1 sncap/3rd_party_app_cifar10:v1.3
+docker push sncap/3rd_party_app_cifar10:v1.3
+docker tag swa/data_processing/cancer:v1.1 sncap/data_processing_cancer:v1.3
+docker push sncap/data_processing_cancer:v1.3
+docker tag swa/data_processing/cifar10:v1.1 sncap/data_processing_cifar10:v1.3
+docker push sncap/data_processing_cifar10:v1.3
 
 echo "Finished push"
 
